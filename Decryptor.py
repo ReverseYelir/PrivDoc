ec6725412c009d0862ca2ba69b3e98b42963ae74
RighteousRiley <rcsimpso@gmail.com>
2022-03-20 01:10:12 -0700

    Removed RSA implementation. Added PBKDF2

--- Decryptor.py
+++ Decryptor.py
@@ -9,16 +9,15 @@ Following Files Needed by Decryptor (same directory):
 '''
 import os
 
-import rsa
 from Crypto.Cipher import AES
-from rsa import DecryptionError
-
+from pbkdf2 import PBKDF2
 
 class Decryptor:
     message_file = None
     private_key = None
+    salt = b''
 
-    def __init__(self, file_path, key_list):
+    def __init__(self, file_path):
         try:
             self.message_file = open(file_path, "rb")
             file_name = os.path.basename(self.message_file.name)
@@ -27,44 +26,33 @@ class Decryptor:
         except FileNotFoundError:
             print("Input File not found. Path Example: C:\\Users\\user\\example.txt")
             exit(1)
-        self.private_key = rsa.PrivateKey(int(key_list[0]), int(key_list[1]), int(key_list[2]), int(key_list[3]), int(key_list[4]))
 
-    def load_privkey(self):
-        key_list = input("Enter Private Key in the form: n, e, d, p, q\n").split(",")
-        return rsa.PrivateKey(int(key_list[0]), int(key_list[1]), int(key_list[2]), int(key_list[3]), int(key_list[4]))
-
-    def load_nonce(self):
-        file = open("nonce.txt", "rb")
-        contents = file.read()
-        file.close()
-        return contents
 
     def load_message(self):
         file = open(self.message_file.name, 'rb')
         contents = file.read()
+        self.salt = contents[-8:]
         file.close()
-        return contents
+        return contents[:-8]
+
+    def create_key(self):
+        first_resp = ""
+        second_resp = " "
+        while first_resp != second_resp:
+            first_resp = input("Enter Encryption Passphrase: ")
+            second_resp = input("Confirm Passphrase: ")
+        key = PBKDF2(first_resp, self.salt).read(32)  # creates 256-bit key
+        return key
 
     def decrypt(self):
-        #  decrypt aes key
-        privkey = self.private_key
-        aes_file = open('aeskey.txt', 'rb')
-        aes_key_ct = aes_file.read()
-        aes_file.close()
-        try:
-            aes_key_pt = rsa.decrypt(aes_key_ct, privkey)
-        except ValueError:
-            print("Invalid Private key paramaters")
-            exit(1)
-        except DecryptionError:
-            print("Decryption error. Likely issue with aeskey's ciphertext")
-            exit(1)
-
-        #  load decryption necessities
-        nonce = self.load_nonce()
         msg_ciphertext = self.load_message()
+        aes_key = self.create_key()
+        cipher = AES.new(aes_key, AES.MODE_EAX, self.salt)
+        try:
+            msg_plaintext = cipher.decrypt(msg_ciphertext).decode()
+        except UnicodeDecodeError:
+            print("Invalid Passphrase")
+            exit(1)
 
-        #  decrypt message
-        cipher = AES.new(aes_key_pt, AES.MODE_EAX)
-        pt = cipher.decrypt(msg_ciphertext)
-        print(f"Decrypted message {pt}\n")
+        print(f"\nDecrypted Message {msg_plaintext}")
+
--- Encryptor.py
+++ Encryptor.py
@@ -13,7 +13,8 @@ Encryption:
         5) output encrypted aeskey to "aeskey.txt"
 '''
 
-import rsa, os
+import os
+from pbkdf2 import PBKDF2
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
@@ -21,8 +22,8 @@ from Crypto.Random import get_random_bytes
 class Encryptor:
     message_file = None
     message_directory = None
-    pubkey = None
-    BIT = 2048
+    SALT_SIZE = 8
+
 
     def __init__(self, file_path):
         try:
@@ -41,27 +42,15 @@ class Encryptor:
         file.write(f"{pubkey.n}, {pubkey.e}")
         file.close()
 
-    def load_pubkey(self):
-        if os.path.exists("pubkey.txt"):
-            file = open("pubkey.txt")
-        else:
-            print(f"pubkey.txt not found. Generating {self.BIT} RSA keys...\n")
-            (pubkey, privkey) = rsa.newkeys(self.BIT)
-            print(f"n: {privkey.n}\n\ne: {privkey.e}\n\nd: {privkey.d}\n\np: {privkey.p}\n\nq: {privkey.q}\n\n")
-            self.pubkey = pubkey
-            self.create_key_file(pubkey)
-            file = open('pubkey.txt', "r")
-        key_list = file.read().split(",")
-        file.close()
-        self.pubkey = rsa.PublicKey(int(key_list[0]), int(key_list[1]))
-
-    def load_privkey(self):
-        key_list = input("Enter Private Key in the form: n, e, d, p, q\n").split(",")
-        return rsa.PrivateKey(int(key_list[0]), int(key_list[1]), int(key_list[2]), int(key_list[3]), int(key_list[4]))
-
-    def load_nonce(self):
-        file = open("nonce.txt", "rb")
-        return file.read()
+    def create_key(self):
+        first_resp = ""
+        second_resp = " "
+        while first_resp != second_resp:
+            first_resp = input("Enter Encryption Passphrase: ")
+            second_resp = input("Confirm Passphrase: ")
+        salt = os.urandom(self.SALT_SIZE)  # creates 64-bit salt
+        key = PBKDF2(first_resp, salt).read(32)  # creates 256-bit key
+        return key, salt
 
     def parse_file_name(self):
         name_list = self.message_file.name.split("\\")
@@ -69,16 +58,9 @@ class Encryptor:
         return name
 
     def encrypt(self):
-        #  Generate AES key and cipher obj
-
-        aes_key_pt = get_random_bytes(32)
-        aes_cipher_obj = AES.new(aes_key_pt, AES.MODE_EAX)
-
-        #  Stores nonce for decryption later
-        nonce = aes_cipher_obj.nonce
-        nonce_file = open("nonce.txt", "wb")
-        nonce_file.write(nonce)
-        nonce_file.close()
+        # grab password and hash via PBKDF2
+        aes_key, salt = self.create_key()
+        aes_cipher_obj = AES.new(aes_key, AES.MODE_EAX, salt)
 
         #  Read in message and encrypt with AES
         msg_file = self.message_file
@@ -89,17 +71,6 @@ class Encryptor:
         #  write out encrypted file
         out_name = f"encrypted-{self.parse_file_name()}"
         file_out = open(out_name, "wb")
-        file_out.write(msg_ciphertext)
+        file_out.write(msg_ciphertext+salt)
         file_out.close()
 
-        # encrypt aes key with rsa
-        self.load_pubkey()
-        print(self.pubkey)
-        aes_key_ct = rsa.encrypt(aes_key_pt, self.pubkey)
-
-        # write aeskey ciphertext
-        file_out = open('aeskey.txt', 'wb')
-        file_out.write(aes_key_ct)
-        file_out.close()
-
-        print(f"Encryption Successful. See '{out_name}'")
