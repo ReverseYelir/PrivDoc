47b6e16cb9c9a977e94745d5dcc8bfeb59158336
RighteousRiley <rcsimpso@gmail.com>
2022-03-20 01:09:18 -0700

    Updated main

--- PrivDoc.py
+++ PrivDoc.py
@@ -23,32 +23,31 @@ Usage:
     -enc: Flag notifying you want to ENCRYPT the file
     -dec: Flag notifying you want to DECRYPT the file
 '''
-import sys, os
-import traceback
+import sys
 
-from time import sleep
 import Encryptor
 import Decryptor
 
 def run_encryption(args):
     # encryption
-    print(f"enc args {args}")
     absolute_path = args[1]
     enc = Encryptor.Encryptor(absolute_path)
     enc.encrypt()
 
 def run_decryption(args):
-
     absolute_path = args[1]
-    try:
-        key_list = [args[2], args[3], args[4], args[5], args[6]]
-    except IndexError:
-        print("Invalid key. Need n, e, d, p, q")
-        exit(1)
-    dec = Decryptor.Decryptor(absolute_path, key_list)
+    dec = Decryptor.Decryptor(absolute_path)
     dec.decrypt()
 
-def parse_args():
+'''
+return [absolute_path]
+return [absolute_path, privatekey_string]
+    privatekey_string: "n, e, d, p, q"
+'''
+
+
+
+def main():
     args = sys.argv
     print(args)
     cmd_str = args[1].rstrip(" ").lstrip(" ")
@@ -60,48 +59,6 @@ def parse_args():
     else:
         print("User error or need to ask for input directly")
         exit(1)
-
-'''
-return [absolute_path]
-return [absolute_path, privatekey_string]
-    privatekey_string: "n, e, d, p, q"
-'''
-def fetch_input():
-    print("Absolute Path Ex: C:\\Users\\user\\input.txt\n")
-    file_path = input("Enter Absolute Path for file to Encrypt or Decrypt: ").lstrip().rstrip()
-    if not os.path.exists(file_path):
-        print("File Not Found. Ex: C:\\Users\\user\\input.txt\nNote the slash direction")
-        exit(1)
-
-    mode = input("Do you want to Encrypt or Decrypt? (E/D)?").lower()
-    if mode == 'e':
-        return [file_path]
-    elif mode == 'd':
-        print("Gathering PrivateKey information for Decryption...\n")
-        try:
-            n = int(input("Enter n: "))
-            e = int(input("Enter e: "))
-            d = int(input("Enter d: "))
-            p = int(input("Enter p: "))
-            q = int(input("Enter q: "))
-        except ValueError:
-            print("Please enter valid integers for your private key.")
-            exit(1)
-        key_list= [n, e, d, p, q]
-        return [file_path, key_list]
-
-
-def main():
-    # user_args = fetch_input()
-    # if len(user_args) == 1:
-    #     run_encryption(user_args)
-    # elif len(user_args) == 2:
-    #     try:
-    #         run_decryption(user_args)
-    #     except:
-    #         traceback.print_ecx()
-    # input()
-    parse_args()
 
 
 if __name__ == "__main__":
